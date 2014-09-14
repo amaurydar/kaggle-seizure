0f93f43a779bfbb562f17355591bf4495befc109
Momo <amaury.darcet@polytechnique.edu>
2014-09-13 23:25:14 -0400

    Script tests verifiant le bon fonctionnement du chargement des donnees

--- tests.py
+++ tests.py
@@ -5,5 +5,14 @@ dog1 = data.Subject('Dog', 1)
 i = 0
 for segment in dog1.hour_segments('preictal'):
     i += 1
+print 'Nombre de segments d\'une heure pour Dog_1 : %s' % i
 
-print i
+print '---'
+
+for segment in dog1.hour_segments('preictal'):
+    print 'Premier segment d\'une heure de Dog_1 :'
+    print 'Data (matrice de taille %s) :' % str(segment.data.shape)
+    print segment.data
+    print 'Duree : %s secondes' % segment.length
+    print 'Temps entre le debut du segment et la seizure : %s secondes' % segment.time_to_seizure
+    break
