16dca154f57de771c278ce276448fb3f20955fb4
Momo <amaury.darcet@polytechnique.edu>
2014-09-14 00:55:13 -0400

    Methode permettant de parcourir les donnees d'un sujet sous forme de segments de duree variable

--- common/data.py
+++ common/data.py
@@ -7,10 +7,23 @@ import numpy as np
 
 
 class Segment(object):
-    def __init__(self, data, length, time_to_seizure):
+    def __init__(self, data, duration, time_to_seizure):
         self.data = data
-        self.length = length
+        self.duration = duration
         self.time_to_seizure = time_to_seizure
+
+    def __str__(self):
+        return ('Data shape : %s \n' % str(self.data.shape) +
+                'Duration : %s \n' % self.duration +
+                'Time to seizure : %s' % self.time_to_seizure)
+
+    def subsegment(self, start, length):
+        if (0 <= start) & (start < self.data.shape[1]) & (start + length <= self.data.shape[1]):
+            return Segment(self.data[:, start:(start + length)],
+                           self.duration * (float(length) - 1) / (float(self.data.shape[1]) - 1),
+                           self.time_to_seizure - self.duration * float(start) / (float(self.data.shape[1]) - 1))
+        else:
+            raise Exception("incorrect subsegment bounds")
 
 
 class Subject(object):
@@ -21,11 +34,11 @@ class Subject(object):
             settings = json.load(f)
         self.dir = str(settings['data-dir']) + '/%s_%s' % (self.race, self.n)
         if race == 'Dog':
-            self.inter_time_to_seizure = 604800  # 1semaine
-            self.pre_time_to_seizure = 300  # 5min
+            self.inter_time_to_seizure = float(604800)  # 1semaine
+            self.pre_time_to_seizure = float(300)  # 5min
         elif race == 'Patient':
-            self.inter_time_to_seizure = 14400  # 4h
-            self.pre_time_to_seizure = 300  # 5min
+            self.inter_time_to_seizure = float(14400)  # 4h
+            self.pre_time_to_seizure = float(300)  # 5min
         else:
             raise Exception("incorrect race")
 
@@ -33,7 +46,7 @@ class Subject(object):
         done = False
         i = 0
         while not done:
-            length = 0
+            duration = float(0)
             data = None
             for j in xrange(6):
                 filename = '%s/%s_%s_%s_segment_%04d.mat' % (self.dir, self.race, self.n, type, i * 6 + j + 1)
@@ -43,7 +56,7 @@ class Subject(object):
                     for a in mat.keys():
                         if not '__' in a:
                             name = a
-                    length += mat[name][0][0][1][0][0]
+                    duration += mat[name][0][0][1][0][0]
                     if data is None:
                         data = mat[name][0][0][0]
                     else:
@@ -55,18 +68,28 @@ class Subject(object):
 
             if not done:
                 if type == 'preictal':
-                    time_to_seizure = self.inter_time_to_seizure + 600
+                    time_to_seizure = self.pre_time_to_seizure + float(3600)
                 elif type == 'interictal':
-                    time_to_seizure = self.pre_time_to_seizure + 600
+                    time_to_seizure = self.inter_time_to_seizure + float(3600)
                 elif type == 'test':
                     time_to_seizure = None
                 else:
                     raise Exception("incorrect data in file %s" % filename)
 
-                yield Segment(data, length, time_to_seizure)
+                yield Segment(data, duration, time_to_seizure)
 
                 i += 1
 
+    def segments(self, type, duration):
+        for hour_segment in self.hour_segments(type):
+            length = round(duration / hour_segment.duration * hour_segment.data.shape[1])
+            break
 
+        print 'Duration of the yielded segments is : %s' % (
+            (length - 1) / (hour_segment.data.shape[1] - 1) * hour_segment.duration)
 
-
+        for hour_segment in self.hour_segments(type):
+            i = 0
+            while i + length <= hour_segment.data.shape[1]:
+                yield hour_segment.subsegment(i, length)
+                i += length
--- tests.py
+++ tests.py
@@ -2,17 +2,22 @@ from common import data
 
 dog1 = data.Subject('Dog', 1)
 
+print 'Methode hour_segments :'
 i = 0
 for segment in dog1.hour_segments('preictal'):
     i += 1
-print 'Nombre de segments d\'une heure pour Dog_1 : %s' % i
-
-print '---'
+print 'Nombre de hour_segment pour Dog_1 : %s' % i
 
 for segment in dog1.hour_segments('preictal'):
-    print 'Premier segment d\'une heure de Dog_1 :'
-    print 'Data (matrice de taille %s) :' % str(segment.data.shape)
-    print segment.data
-    print 'Duree : %s secondes' % segment.length
-    print 'Temps entre le debut du segment et la seizure : %s secondes' % segment.time_to_seizure
+    hour_segment1 = segment
     break
+print 'Premier segment d\'une heure de Dog_1 :'
+print hour_segment1
+
+print '---'
+print 'Methode segments :'
+i = 0
+segments = dog1.segments('preictal', 10)
+while i < 3:
+    print segments.next()
+    i += 1
